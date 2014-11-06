40b48e9a041371d808a71152485a508664b07959
xtremyst <xtremyst.in.xtremo@gmail.com>
2014-11-06 20:51:11 +0200

    use python 2 instead, create two seperate functions for radio and interraction

--- myradio2.py
+++ myradio2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import os
 import sys
@@ -9,52 +9,48 @@ names = []
 
 def radio():
     global n, urls, names
-    fhand = open("/home/xtremyst/stations.txt")
+    fhand = open("stations.txt")
+    n = 0
     for line in fhand:
         n += 1
-        print (str(n)+":", line[:line.find(",")])
+        print str(n)+":", line[:line.find(",")]
         url = line[line.find(",") + 1:]
         urls.append(url)
         name = line[:line.find(",")]
         names.append(name)
-    print("")
-    station = input("Select Station or type q to exit: ")
+    
+def interract():
+    print ""
+    station = raw_input("Select Station or type q to exit: ")
     try:
         st = int(station) 
         if st > len(names) or st < 0:
-            n = 0
-            urls = []
-            names = []
-            radio()
+            interract()
     except:
         if station == "q":
-            print("\nGoodbye...\n")
+            print "\nGoodbye...\n"
             sys.exit()
         else:
-            print("\nWrong Input, try again\n")
-            n = 0
-            urls = []        
-            names = []
-            radio()
-    print("","\nNow playing", names[st-1])
-    print ("If you want to stop press q\n")
+            print "\nWrong Input, try again\n"
+            interract()
+    print "\nNow playing", names[st-1]
+    print "If you want to stop press q\n"
     os.system('mplayer -really-quiet '+ urls[st-1])
     make_change()
 
 def make_change():
     global n, names, urls
-    n = 0
-    names = []
-    urls = []
-    change = input("\nType :ch to change station\nType :q to exit\n% ")
-    if change == ":ch":
+    change = raw_input("\nType c to change station\nType q to exit\n% ")
+    if change == "c":
         radio()
-    elif change == ":q":
-        print("\nGoodbye...")
+        interract()
+    elif change == "q":
+        print "\nGoodbye..."
         sys.exit()
     else:
-        print("Wrong Input, try again")
+        print "Wrong Input, try again"
         make_change()
 
 
 radio()
+interract()
